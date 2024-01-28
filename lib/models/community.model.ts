9a199efeb8f90d6948ecaca7db29c8898eb9e405
ianeiji <tajima830@gmail.com>
2024-01-28 13:04:39 +0900

    first commit

---
+++ lib/models/community.model.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const communitySchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  image: String,
+  bio: String,
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  threads: [{ type: mongoose.Schema.Types.ObjectId, ref: "Thread" }],
+  members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+});
+
+const Community =
+  mongoose.models.Community || mongoose.model("Community", communitySchema); // If model exists, use it. If not, create it.
+
+export default Community;
